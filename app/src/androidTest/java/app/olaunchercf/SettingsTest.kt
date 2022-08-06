eefb0e962be6bd3dada7ae0e1dde0d2a14456fe4
Joscha Loos <j.loos@posteo.net>
2022-08-06 02:04:34 +0200

    test: swipe left app

--- app/src/androidTest/java/app/olaunchercf/SettingsTest.kt
+++ app/src/androidTest/java/app/olaunchercf/SettingsTest.kt
@@ -7,12 +7,17 @@ import androidx.navigation.Navigation.findNavController
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.action.ViewActions.longClick
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.CursorMatchers.withRowString
 import androidx.test.espresso.matcher.ViewMatchers.*
+import app.olaunchercf.data.AppModel
 import app.olaunchercf.data.Constants
 import app.olaunchercf.data.Constants.PREFS_FILENAME
+import app.olaunchercf.data.Prefs
+import app.olaunchercf.ui.AppDrawerAdapter
+import org.hamcrest.core.IsAnything.anything
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -50,10 +55,17 @@ class SettingsTest {
         }
     }
 
-    /*@Test
-    fun testHomeApps() {
-        onView(withId(0)).perform(longClick())
-    }*/
+    @Test
+    fun testSwipeApps() {
+        goToSettings()
+        composeTestRule.onNode(hasAnySibling(hasText("Swipe left app"))).performClick()
+        onView(withId(R.id.recyclerView)).perform(
+            RecyclerViewActions
+                .actionOnItemAtPosition<AppDrawerAdapter.ViewHolder>(3, click())
+        )
+        onView(withId(R.id.nav_host_fragment)).perform(swipeLeft())
+        Prefs(composeTestRule.activity).appSwipeLef
+    }
 
     private fun goToSettings() {
         composeTestRule.activityRule.scenario.onActivity {
@@ -63,16 +75,16 @@ class SettingsTest {
     }
 
     private fun increaseAppNumber(i: Int) {
-        composeTestRule.onNode(hasAnySibling(hasText("Apps on home screen"))).performClick()
+        composeTestRule.onNode(hasAnySibling(hasText("Number of shortcuts"))).performClick()
         composeTestRule.onNodeWithText("+").assertIsDisplayed().performClick()
-        composeTestRule.onNodeWithText("Commit").assertIsDisplayed().performClick()
+        composeTestRule.onNodeWithText("Save").assertIsDisplayed().performClick()
         composeTestRule.onAllNodesWithText((i+1).toString())[0].assertIsDisplayed()
     }
 
     private fun decreaseAppNumber(i: Int) {
         composeTestRule.onAllNodesWithText(i.toString())[0].performClick()
         composeTestRule.onNodeWithText("-").assertIsDisplayed().performClick()
-        composeTestRule.onNodeWithText("Commit").assertIsDisplayed().performClick()
+        composeTestRule.onNodeWithText("Save").assertIsDisplayed().performClick()
         composeTestRule.onAllNodesWithText((i-1).toString())[0].assertIsDisplayed()
     }
 }

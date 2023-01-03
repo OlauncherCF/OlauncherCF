a592da483e0865942cc07896d729dfa83a7a3b58
The-Repo-Club <wayne6324@gmail.com>
2023-01-03 20:21:55 +0000

    feat: move over to kts file if you want too

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    val kotlinVersion = "1.7.10"
+    extra ["kotlinVersion"] = kotlinVersion
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = kotlinVersion))
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+plugins {
+    id ("com.android.application") version "7.3.1" apply false
+    id ("com.android.library") version "7.3.1" apply false
+}
+
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.buildDir)
+ }
